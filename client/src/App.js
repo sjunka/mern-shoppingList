45ef9ec5a326127f4cc256fb9f3f5b746c20b967
Sergio Junca <sergio.junca@gmail.com>
2018-06-29 19:08:03 -0500

    Created ShoppingList Component, added static state, added transitions, added add & remove buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
 import AppNavBar from "./components/AppNavbar";
+import ShoppingList from "./components/ShopingList";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-
-      <Fragment>
-        <AppNavBar/>
-
-      </Fragment>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <AppNavBar />
+                <ShoppingList />
+            </Fragment>
+        );
+    }
 }
 
 export default App;
