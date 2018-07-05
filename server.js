6e78ab794fb63ac67e97a1f3d84d8dbf2c27372f
Sergio Junca <sergio.junca@gmail.com>
2018-07-05 11:10:28 -0500

    Added post build script

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 // Sending info from client to server
 const bodyParser = require("body-parser");
+// Path : used to deal with file paths
+const path = require("path");
 
 // API routes 
 const item = require("./routes/api/items");
@@ -26,6 +28,21 @@ mongoose
 
 // Use Routes from the API
 app.use("/api/items", item);
+
+
+// Deployment script: Serve static asset if ENV === production
+if ( process.env.NODE_ENV === "production" ) {
+    // Set a static folder 
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
+
+
+
 
 // Defining application port:
 const port = process.env.PORT || 5000;
